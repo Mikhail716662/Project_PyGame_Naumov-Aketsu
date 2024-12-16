ef8d06e79642a7b8a970dfcc3db1bdfb5a796dce
Mikhail <numizmataleks@gmail.com>
2024-12-16 19:03:51 +0300

    Добавлены заготовки для классов

---
+++ data/classes.py
@@ -0,0 +1,34 @@
+class Main_Menu:
+    pass
+
+
+class Player:
+    pass
+
+
+class Spike:
+    pass
+
+
+class Lava:
+    pass
+
+
+class End_Level_Menu:
+    pass
+
+
+class Records_Menu:
+    pass
+
+
+class Settings_Menu:
+    pass
+
+
+class Platform:
+    pass
+
+
+class Finish_Point:
+    pass
