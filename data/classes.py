f1f5356b1b2643c7abe5e2be061141dde84450bd
Mikhail <aksios705@yandex.ru>
2025-01-12 16:01:30 +0300

    Добавил фон

--- data/classes.py
+++ data/classes.py
@@ -3,8 +3,14 @@ import time
 import pygame
 
 
-class Main_Menu:
-    pass
+class Background(pygame.sprite.Sprite):
+    def __init__(self, size):
+        super().__init__()
+        self.image = pygame.image.load("images/image1.png")
+        # self.image = pygame.transform.rotozoom(self.image, 0, 1.94)
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.bottom = size[1]
 
 
 class Player(pygame.sprite.Sprite):
@@ -12,15 +18,15 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load('images/image.png')
         self.rect = self.image.get_rect(center=(x, 0))
-        self.rect.center = (1280 / 2, 1024 / 2)
+        self.rect.center = (1280 / 2, 720 / 2)
 
     def update(self):
         self.speedx = 0
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
-            self.speedx = -3
+            self.speedx = -15
         if key[pygame.K_RIGHT]:
-            self.speedx = 3
+            self.speedx = 15
         self.rect.x += self.speedx
 
 
--- data/functions.py
+++ data/functions.py
@@ -2,15 +2,18 @@ import random
 
 import pygame
 
-from data.classes import Player
+from data.classes import Player, Background
 
 
 def main():
     pygame.init()
-    size = 1280, 1024
+    size = 1280, 720
     screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Приключение зеленухи')
     clock = pygame.time.Clock()
     all_sprites = pygame.sprite.Group()
+    background = Background(size)
+    all_sprites.add(background)
     player = Player(random.randint(1, size[0]))
     all_sprites.add(player)
     running = True
@@ -20,7 +23,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill("gray")
+        screen.blit(background.image, background.rect)
         screen.blit(player.image, player.rect)
         pygame.display.update()
         pygame.display.flip()
